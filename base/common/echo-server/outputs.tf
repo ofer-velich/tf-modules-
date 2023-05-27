36e6862159c0b7ab70a2955c82c83a55a20f896c
Ofer Velich <oferv@wix.com>
2023-05-27 14:08:51 +0300

    example

---
+++ base/common/echo-server/outputs.tf
@@ -0,0 +1,7 @@
+output "echo1" {
+  value = var.echo1
+}
+
+output "echo2" {
+  value = var.echo2
+}
---
+++ base/common/echo-server/variables.tf
@@ -0,0 +1,14 @@
+################################################################################################################
+#                                                 Input Variables
+################################################################################################################
+variable echo1 {
+  type = string
+  description = "(Required) variable to echo"
+}
+
+variable echo2 {
+  type = string
+  default = ""
+  description = "(Optional) variable to echo"
+}
+
