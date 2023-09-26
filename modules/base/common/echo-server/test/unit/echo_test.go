6eef1ff893d538a857ab3018ad3a3b49ab0306a5
oferwix <97063582+oferwix@users.noreply.github.com>
2023-09-26 15:52:04 +0300

    Create echo_test.go

---
+++ modules/base/common/echo-server/test/unit/echo_test.go
@@ -0,0 +1,26 @@
+/*
+commands to run
+    go mod init "test"
+    go mod tidy
+    go test
+*/
+package test
+
+import (
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTerraformEchoServerModule(t *testing.T) {
+	// Construct the terraform options with default retryable errors to handle the most common
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// Set the path to the Terraform code that will be tested.
+		TerraformDir: "./complete",
+	})
+
+	// Run "terraform init" and "terraform plan". Fail the test if there are any errors.
+	_, err := terraform.InitAndPlanE(t, terraformOptions)
+	require.NoError(t, err)
+}
